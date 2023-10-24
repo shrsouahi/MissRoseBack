1e0ab1638912f1059ab19c909603c78e6c8cabe0
shrsouahi <sahar.souahi@istic.ucar.tn>
2023-10-24 10:30:56 +0100

    first commit
    none

---
+++ src/main/java/com/project/miss/entites/SignInRequest.java
@@ -0,0 +1,33 @@
+package com.project.miss.entites;
+
+public class SignInRequest {
+	
+    private String email;
+    private String password;
+
+    public SignInRequest() {
+        // Default constructor
+    }
+
+    public SignInRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
---
+++ src/main/java/com/project/miss/service/InterUserService.java
@@ -0,0 +1,30 @@
+package com.project.miss.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.project.miss.entites.User;
+
+
+
+public interface InterUserService {
+	
+	
+	public User getUserByEmailAndPassword(String email, String pwd);
+	
+	public List<User> getAll();
+	
+	public User addUser(User user);
+	
+	public String deleteUserById( Long iduser);
+	
+	public User updateUser(User user, Long iduser);
+	
+	public Optional<User> getUserById(Long iduser);
+	
+	public List<User> getUsersByRole(String roleName);
+	
+	public User authenticateUser(String email, String password);
+
+
+}
